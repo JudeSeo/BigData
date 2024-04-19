3932b62ea3d079f4c527b4cd2c74d34bf7ecb2c7
JudeSeo <gn0408@gmail.com>
2024-04-19 21:57:41 +0900

    빅분기 131

---
+++ 202404/131.py
@@ -0,0 +1,19 @@
+# 01
+a = 1234
+b = -12.34
+c = a + b;
+print(a, b);
+print(type(1), type(b));
+print(c);
+print(type(c));
+
+# 02
+a = 3;
+b = 7;
+print(a + b);
+print(a - b);
+print(a * b);
+print(a / b); # 나누기
+print(a ** b); # 제곱
+print(a % b); # 나머지
+print(a // b); #몫
