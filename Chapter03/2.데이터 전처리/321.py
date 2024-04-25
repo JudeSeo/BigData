3ad5ff75e254608d9472817bbc5641f9d7f57cf5
JudeSeo <gn0408@gmail.com>
2024-04-25 21:37:20 +0900

    빅분기 321

---
+++ Chapter03/2.데이터 전처리/321.py
@@ -0,0 +1,17 @@
+# 데이터 확인
+
+import pandas as pd;
+
+data = pd.read_csv("../1.데이터 수집/housing_data.csv", header=None, sep=",");
+col_names = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV',
+             'isHighValue'];
+data.columns = col_names
+
+# 01
+print(data.shape);
+
+# 02
+print(data.info());
+
+# 03
+print(data.describe());
