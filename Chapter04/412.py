a64773a0754f70ec6461a8ae45c2d50b6a7ea869
JudeSeo <gn0408@gmail.com>
2024-05-16 22:21:00 +0900

    빅분기 412

---
+++ Chapter04/412.py
@@ -0,0 +1,18 @@
+# 사전 준비
+
+# 01
+import pandas as pd;
+import numpy as np;
+
+X_test = pd.read_csv("./yemoonsaBigdata/datasets/Part2/mpg_X_test.csv");
+X_train = pd.read_csv("./yemoonsaBigdata/datasets/Part2/mpg_X_train.csv");
+y_train = pd.read_csv("./yemoonsaBigdata/datasets/Part2/mpg_y_train.csv");
+
+# 02
+import pandas as pd;
+import numpy as np;
+
+# 03
+X_test = pd.read_csv("./yemoonsaBigdata/datasets/Part2/mpg_X_test.csv");
+X_train = pd.read_csv("./yemoonsaBigdata/datasets/Part2/mpg_X_train.csv");
+y_train = pd.read_csv("./yemoonsaBigdata/datasets/Part2/mpg_y_train.csv");
