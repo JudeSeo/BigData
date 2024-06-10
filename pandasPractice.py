23ac8f4e7979d369a07b670d9e6d90db369a16a7
JudeSeo <gn0408@gmail.com>
2024-06-10 18:36:09 +0900

    빅분기 판다스연습 ~ #25

--- pandasPractice.py
+++ pandasPractice.py
@@ -1,5 +1,3 @@
-# 2024-06-09
-
 import pandas as pd
 
 df = pd.read_csv("https://raw.githubusercontent.com/Datamanim/pandas/main/lol.csv", sep="\t")
@@ -27,7 +25,40 @@ df = pd.read_csv('https://raw.githubusercontent.com/Datamanim/pandas/main/Jeju.c
 
 # print(df.info())
 
-print(df.select_dtypes(exclude='object').columns);
-print(df.select_dtypes(include='object').columns);
+# print(df.select_dtypes(exclude='object').columns);
+# print(df.select_dtypes(include='object').columns);
 
-df.isnull().sum(axis=0);
+# df.isnull().sum(axis=0);
+
+# df.info();
+
+# display(df.describe());
+
+# print(df['거주인구']);
+
+# quantile 사분위 수
+# print(df['평균 속도'].quantile(0.75)-df['평균 속도'].quantile(0.25));
+
+# nunique 유일값 수
+# df['읍면동명'].nunique()
+
+# unique 유일값
+# df['읍면동명'].unique()
+
+df = pd.read_csv('https://raw.githubusercontent.com/Datamanim/pandas/main/chipo.csv');
+# type(df);
+
+# loc
+# df.loc[df['quantity']==3].head(5);
+
+# reset_index(drop=True) 인덱스 처음부터 생성
+# print(df.loc[df['quantity']==3].head(5).reset_index(drop=True))
+
+# print(df[['quantity', 'item_price']]);
+
+df['new_price'] = df['item_price'].str[1:].astype('float');
+# print(df['new_price'].head());
+# df.info();
+
+# print(df.loc[df['new_price']<=5]);
+# print(len(df.loc[df['new_price']<=5]));
