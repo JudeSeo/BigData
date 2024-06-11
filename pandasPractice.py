3d2d373f3d46bb96fe2b4823a55ca4f8ea86774e
JudeSeo <gn0408@gmail.com>
2024-06-11 20:28:33 +0900

    빅분기 판다스연습 ~ #43

--- pandasPractice.py
+++ pandasPractice.py
@@ -73,4 +73,51 @@ df['new_price'] = df['item_price'].str[1:].astype('float');
 # print(df.loc[df['item_name'].str.contains('Chips')]);
 
 # 짝수번째 컬럼만 출력
-# print(df.iloc[:,::2]);
+# print(df);
+# display(df.iloc[:,::2]);
+
+# df의 new_price 컬럼 값에 따라 내림차순으로 정리하고 index를 초기화 하여라
+# display(df.sort_values('new_price', ascending=False).reset_index(drop=True));
+
+
+# df의 item_name 컬럼 값이 Steak Salad 또는 Bowl 인 데이터를 인덱싱하라
+# display(df.loc[(df['item_name'] == 'Steak Salad') | (df['item_name'] == 'Bowl')]);
+
+# df의 item_name 컬럼 값이 Steak Salad 또는 Bowl 인 데이터를 데이터 프레임화 한 후, item_name를 기준으로 중복행이 있으면 제거하되 첫번째 케이스만 남겨라
+# display(df.loc[(df['item_name'] == 'Steak Salad') | (df['item_name'] == 'Bowl')].drop_duplicates('item_name'));
+
+# df의 item_name 컬럼 값이 Steak Salad 또는 Bowl 인 데이터를 데이터 프레임화 한 후, item_name를 기준으로 중복행이 있으면 제거하되 마지막 케이스만 남겨라
+# display(df.loc[(df['item_name'] == 'Steak Salad') | (df['item_name'] == 'Bowl')].drop_duplicates('item_name', keep='last'));
+
+# df의 데이터 중 new_price값이 new_price값의 평균값 이상을 가지는 데이터들을 인덱싱하라
+# display(df.loc[df['new_price'] >= df['new_price'].mean()]);
+
+# df의 데이터 중 item_name의 값이 Izze 데이터를 Fizzy Lizzy로 수정하라
+# df.loc[df['item_name'] == 'Izze', 'item_name'] = 'Fizzy Lizzy';
+# display(df);
+
+# df의 데이터 중 choice_description 값이 NaN 인 데이터의 갯수를 구하여라
+# display(df.choice_description.isnull().sum());
+
+# df의 데이터 중 choice_description 값이 NaN 인 데이터를 NoData 값으로 대체하라(loc 이용)
+df.loc[df.choice_description.isnull(), 'choice_description'] = 'NoData'
+# display(df)
+
+# df의 데이터 중 choice_description 값에 Black이 들어가는 경우를 인덱싱하라
+# display(df.loc[df.choice_description.str.contains('Black')])
+# 값에 null이 있으면 에러남
+
+# df의 데이터 중 choice_description 값에 Vegetables 들어가지 않는 경우의 갯수를 출력하라
+# display(len(df.loc[~df.choice_description.str.contains('Vegetables')]))
+
+# df의 데이터 중 item_name 값이 N으로 시작하는 데이터를 모두 추출하라
+# display(df[df.item_name.str.startswith('N')]);
+
+# df의 데이터 중 item_name 값의 단어갯수가 15개 이상인 데이터를 인덱싱하라
+# display(df[df.item_name.str.len() >= 15]);
+
+# df의 데이터 중 new_price값이 lst에 해당하는 경우의 데이터 프레임을 구하고 그 갯수를 출력하라 lst =[1.69, 2.39, 3.39, 4.45, 9.25, 10.98, 11.75, 16.98]
+# list = [1.69, 2.39, 3.39, 4.45, 9.25, 10.98, 11.75, 16.98];
+# ans = df[df.new_price.isin(list)];
+# display(ans)
+# display(len(ans));
