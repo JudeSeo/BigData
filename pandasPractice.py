c35fc7518c68a0735bccad42d33e91cf67c9c42b
JudeSeo <gn0408@gmail.com>
2024-06-13 19:54:54 +0900

    빅분기 판다스연습 ~ #63

--- pandasPractice.py
+++ pandasPractice.py
@@ -160,7 +160,93 @@ df = pd.read_csv('https://raw.githubusercontent.com/Datamanim/pandas/main/AB_NYC
 # 데이터중 neighbourhood_group 값이 Queens값을 가지는 데이터들 중 neighbourhood 그룹별로 price값의 평균, 분산, 최대, 최소값을 구하라
 # display(df[df.neighbourhood_group == "Queens"].groupby(['neighbourhood']).agg({'price':['mean', 'var','max','min']}));
 
+# *************************************************
 # 데이터중 neighbourhood_group 값에 따른 room_type 컬럼의 숫자를 구하고 neighbourhood_group 값을 기준으로 각 값의 비율을 구하여라
-ans = df[['neighbourhood_group', 'room_type']].groupby(['neighbourhood_group', 'room_type']).size().unstack();
-ans.loc[:,:]=(ans.values/ans.sum(axis=1).values.reshape(-1,1))
-display(ans);
+# ans = df[['neighbourhood_group', 'room_type']].groupby(['neighbourhood_group', 'room_type']).size().unstack();
+# ans.loc[:,:]=(ans.values/ans.sum(axis=1).values.reshape(-1,1))
+# display(ans);
+
+# ~55
+
+
+
+# 데이터를 로드하고 데이터 행과 열의 갯수를 출력하라
+df= pd.read_csv('https://raw.githubusercontent.com/Datamanim/pandas/main/BankChurnersUp.csv');
+# display(df.shape);
+
+# Income_Category의 카테고리를 map 함수를 이용하여 다음과 같이 변경하여 newIncome 컬럼에 매핑하라
+# Unknown : N
+# Less than $40K : a
+# $40K - $60K : b
+# $60K - $80K : c
+# $80K - $120K : d
+# $120K +’ : e
+
+# dic={
+# 'Unknown' : 'N',
+# 'Less than $40K' : 'a',
+# '$40K - $60K' : 'b',
+# '$60K - $80K' : 'c',
+# '$80K - $120K' : 'd',
+# '$120K +' : 'e',
+# }
+# df['newIncome'] = df.Income_Category.map(lambda x:dic[x]);
+# display(df['newIncome'])
+
+
+# Income_Category의 카테고리를 apply 함수를 이용하여 다음과 같이 변경하여 newIncome 컬럼에 매핑하라
+# Unknown : N
+# Less than $40K : a
+# $40K - $60K : b
+# $60K - $80K : c
+# $80K - $120K : d
+# $120K +’ : e
+
+# def changeCategory(x):
+#     if x =='Unknown':
+#         return 'N'
+#     elif x =='Less than $40K':
+#         return 'a'
+#     elif x =='$40K - $60K':
+#         return 'b'
+#     elif x =='$60K - $80K':
+#         return 'c'
+#     elif x =='$80K - $120K':
+#         return 'd'
+#     elif x =='$120K +' :
+#         return 'e'
+
+# df['new_Income'] = df.Income_Category.apply(lambda x:changeCategory(x));
+# display(df['new_Income']);
+
+
+# Customer_Age의 값을 이용하여 나이 구간을 AgeState 컬럼으로 정의하라. (0~9 : 0 , 10~19 :10 , 20~29 :20 … 각 구간의 빈도수를 출력하라
+# df['AgeState'] = df.Customer_Age.map(lambda x:x//10*10);
+# display(df.groupby('AgeState').size());
+
+# Education_Level의 값중 Graduate단어가 포함되는 값은 1 그렇지 않은 경우에는 0으로 변경하여 newEduLevel 컬럼을 정의하고 빈도수를 출력하라
+# df['newEduLevel'] = df.Education_Level.map(lambda x: 1 if "Graduate" in x else 0);
+# display(df.groupby('newEduLevel').size());
+# display(df['newEduLevel'].value_counts());
+
+# Credit_Limit 컬럼값이 4500 이상인 경우 1 그외의 경우에는 모두 0으로 하는 newLimit 정의하라. newLimit 각 값들의 빈도수를 출력하라
+# df['newLimit'] = df.Credit_Limit.map(lambda x: 1 if x >= 4500 else 0);
+# display(df['newLimit'].value_counts());
+
+# Marital_Status 컬럼값이 Married 이고 Card_Category 컬럼의 값이 Platinum인 경우 1 그외의 경우에는 모두 0으로 하는 newState컬럼을 정의하라. newState의 각 값들의 빈도수를 출력하라
+# def changeCategory(x):
+#     if x.Marital_Status =='Married' and x.Card_Category == 'Platinum':
+#         return 1
+#     else:
+#         return 0
+
+# df['newState'] = df.apply(changeCategory, axis=1);
+# display(df['newState'].value_counts());
+
+# Gender 컬럼값 M인 경우 male F인 경우 female로 값을 변경하여 Gender 컬럼에 새롭게 정의하라. 각 value의 빈도를 출력하라
+# def changeGender(x):
+#     if x == 'M': return 'male';
+#     else: return 'female';
+
+# df['Gender'] = df.Gender.apply(changeGender);
+# display(df['Gender'].value_counts());
