4d20f7c4234b86ba06611dbd49be127e7dccbc3e
JudeSeo <gn0408@gmail.com>
2024-06-18 21:17:34 +0900

    빅분기 판다스연습 ~ #95

--- pandasPractice.py
+++ pandasPractice.py
@@ -382,5 +382,42 @@ df = pd.read_csv('https://raw.githubusercontent.com/Datamanim/pandas/main/winter
 # display(df.pivot_table(index='Sport',columns='Gender', aggfunc='size'));
 
 # 전체 데이터에서 Discipline종류에 따른 따른 Medal수를 구하여라
-display(df.pivot_table(index='Discipline',columns='Medal', aggfunc='size'));
+# display(df.pivot_table(index='Discipline',columns='Medal', aggfunc='size'));
 
+
+df = pd.read_csv('https://raw.githubusercontent.com/Datamanim/pandas/main/mergeTEst.csv',index_col= 0);
+
+df1 = df.iloc[:4,:]
+df2 = df.iloc[4:,:]
+
+# display(df1)
+# display(df2)
+
+# df1과 df2 데이터를 하나의 데이터 프레임으로 합쳐라
+# display(pd.concat([df1,df2]));
+
+df3 = df.iloc[:2,:4]
+df4 = df.iloc[5:,3:]
+
+# display(df3);
+# display(df4);
+
+# df3과 df4 데이터를 하나의 데이터 프레임으로 합쳐라. 둘다 포함하고 있는 년도에 대해서만 고려한다
+# help(pd.concat)
+# display(pd.concat([df3, df4], join='inner'));
+
+# df3과 df4 데이터를 하나의 데이터 프레임으로 합쳐라. 모든 컬럼을 포함하고, 결측치는 0으로 대체한다
+# display(pd.concat([df3, df4], join='outer').fillna(0));
+
+df5 = df.T.iloc[:7,:3]
+df6 = df.T.iloc[6:,2:5]
+
+# display(df5)
+# display(df6)
+
+# df5과 df6 데이터를 하나의 데이터 프레임으로 merge함수를 이용하여 합쳐라. Algeria컬럼을 key로 하고 두 데이터 모두 포함하는 데이터만 출력하라
+# help(pd.merge);
+# display(pd.merge(df5,df6, how='inner', on='Algeria'));
+
+# df5과 df6 데이터를 하나의 데이터 프레임으로 merge함수를 이용하여 합쳐라. Algeria컬럼을 key로 하고 합집합으로 합쳐라
+# display(pd.merge(df5, df6, on='Algeria', how='outer'));
