5cbb638aa6790d2742054ea502b8cbfcc4e60dc1
JudeSeo <gn0408@gmail.com>
2024-10-03 19:59:37 +0900

    폴더정리

--- Book/Chapter03/1.데이터 수집/312.py
+++ Book/Chapter03/1.데이터 수집/312.py
@@ -3,7 +3,7 @@
 # 01
 import pandas as pd;
 
-data = pd.read_csv("./housing_data.csv", header=None, sep=",");
+data = pd.read_csv("housing_data.csv", header=None, sep=",");
 col_names = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV',
              'isHighValue'];
 data.columns = col_names
